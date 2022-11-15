35b4c70f6081a67136663b5a82bee23cd60b84d0
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-15 11:59:58 +0200

    feat(25): solved task

--- JavaScript essentials [24-49]/25 - data types, operators and objects/main.js
+++ JavaScript essentials [24-49]/25 - data types, operators and objects/main.js
@@ -8,3 +8,17 @@ const age3 = '42';
 //  16.999 -> 16
 //  16.001 -> 16
 
+// var1 - aiškus skaitymas, nuoseklu.
+{
+  const count = 3;
+  const sum = age1 + age2 + Number(age3);
+  const avg = sum / count;
+  const avgRounded = Math.floor(avg);
+  
+  console.log('avg:', avgRounded);
+}
+// var 2 - taumpomi minimalūs resursai.
+{
+  console.log('avg:', Math.floor((age1 + age2 + Number(age3)) / 3));
+}
+
