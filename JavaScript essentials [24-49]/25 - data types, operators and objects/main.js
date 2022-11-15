ec8177c3b813181f76f27de04ef083e41e37a8f5
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-15 11:48:25 +0200

    feat(25): mini task

---
+++ JavaScript essentials [24-49]/25 - data types, operators and objects/main.js
@@ -0,0 +1,10 @@
+const age1 = 13;
+const age2 = 64;
+const age3 = '42';
+
+// Sukurkite kintamajį, kuriame apskaičiuotumet amžiaus vidurkį.
+// Suapvalintą į žemesniają skaičiau dalį. Rezultatą atspausdinkite Konsolėje:
+//  16.5 -> 16
+//  16.999 -> 16
+//  16.001 -> 16
+
