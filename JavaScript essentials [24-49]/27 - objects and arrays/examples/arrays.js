64c6a8b88e2d116f23db3fddd84c30e6b5af1dfa
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-17 10:37:31 +0200

    feat(27): task array tasks with sentences

--- JavaScript essentials [24-49]/27 - objects and arrays/examples/arrays.js
+++ JavaScript essentials [24-49]/27 - objects and arrays/examples/arrays.js
@@ -25,15 +25,3 @@ numbers[Math.floor((numbers.length) / 2)] // Vidurinis masyvo elementas elementa
 let i = 3,
   j = 4;
 numbers[i % 2 + j++]; // Sudėtinė indekso skaičiavimo logika.
-
-// Užduotis: Sudarykite šiuos sakinius ir išsaugokite kitamuosiuose:
-//    sentence1, sentence2, ... , sentenceN;  N - natūralieji skaičiai.
-// 1. my sister is in the lake
-let sentence1 = helpers[3] + helpers[7] + nouns[1] + helpers[7] + helpers[4] + helpers[7] + helpers[0] + helpers[7] + helpers[6] + helpers[7] + nouns[6];
-//                'my'         ' '        'sister'      ' '          'is'          ' '         'in'          ' '         'the'         ' '       'lake'                                                                               
-console.log(sentence1);
-// 2. hammer is on the table
-// 3. sun is in the sky
-// 4. apple is with my sister
-// 5. Papildyti masyvą žodžiais, ir sukurti dar 3 +-logiškus sakinius.
-
---
+++ JavaScript essentials [24-49]/27 - objects and arrays/main.js
@@ -0,0 +1,5 @@
+const a = 7;
+const b = -17;
+const c = 22;
+
+console.log({ a, b, c });
