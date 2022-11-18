2b9b4fbb42361afd0b98b6ffec0de93964b81ab1
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-18 11:15:18 +0200

    feat: solved lesson tasks

--- JavaScript essentials [24-49]/28 - functions/main.js
+++ JavaScript essentials [24-49]/28 - functions/main.js
@@ -4,31 +4,54 @@
 console.group('1. Parašykite funkciją, kuri atspausdintų bet kokį string"ą 2 kartus, skirtingose eilutėse');
 {
   // funkcijos deklaracija
-
-  // bandomieji kintamieji
+  function repeat(text) {
+    console.log(text);
+    console.log(text);
+  }
+  // bandomieji kintamiejiconst 
+  const str1 = 'labas';
+  const str2 = 'ate';
 
   // rezultatų spausdinimas 
+  repeat(str1);
+  repeat(str2);
 
 }
 console.groupEnd();
 console.group('2. Parašykite funkciją, kuri grąžintų bet kokį string"ą 5 kartus, atskirtus tarpu');
 {
   // funkcijos deklaracija
+  function repeatFiveTimesInRow(text) {
+    return `${text} ${text} ${text} ${text} ${text}`;
+  }
 
-  // bandomieji kintamieji
+  // bandomieji kintamiejiconst 
+  const str1 = 'labas';
+  const str2 = 'ate';
 
   // funkcijos iškvietimai ir saugojimai į rezultatus
-
+  const res1 = repeatFiveTimesInRow(str1);
+  const res2 = repeatFiveTimesInRow(str2);
   // rezultatų spausdinimas 
 
+  console.table({ res1, res2 });
 }
 console.groupEnd();
 console.group('3. Parašykite funkciją, kuri atspausdintų bet kokį string"ą, bet kokį kiekį kartų, askirtus tarpu - vienoje eilutėje');
 {
   // funkcijos deklaracija
   function printStringNTimesInRow(text, n) {
-    const textWithSpace = text + ' ';
-    const result = textWithSpace.repeat(n);
+    let result = '';
+
+    for (let i = 0; i < n; i += 1) {
+      const isLastElement = i === n - 1;
+
+      if (isLastElement) {
+        result += text;
+      } else {
+        result += text + ' ';
+      }
+    }
 
     console.log(result);
   }
@@ -50,20 +73,64 @@ console.groupEnd();
 console.group('4. Parašykite funkciją, kuri atspausdintų bet kokį string"ą, bet kokį kiekį kartų, askirtus programuotojo nurodytu simboliu - vienoje eilutėje');
 {
   // funkcijos deklaracija
+  function printStringNTimesInRow(text, n, separator) {
+    let result = '';
+
+    for (let i = 0; i < n; i += 1) {
+      const isLastElement = i === n - 1;
+
+      result += text;
+      if (!isLastElement) {
+        result += separator;
+      }
+    }
+
+    console.log(result);
+  }
 
   // bandomieji kintamieji
+  const str1 = 'labas';
+  const n1 = 3;
+  const str2 = 'ate';
+  const n2 = 2;
+  const str3 = 'gyvate';
+  const n3 = 7;
 
-  // rezultatų spausdinimas 
+  // rezultatų spausdinimas
+  printStringNTimesInRow(str1, n1, '*');
+  printStringNTimesInRow(str2, n2, '-');
+  printStringNTimesInRow(str3, n3, '_');
 }
 console.groupEnd();
 console.group('5. Parašykite funkciją, kuri grąžintų bet kokį string"ą, bet kokį kiekį kartų, skirtingose eilutėse');
-{
-  // funkcijos deklaracija
+{  // funkcijos deklaracija
+  function printStringNTimesInRow(text, n) {
+    let result = '';
+
+    for (let i = 0; i < n; i += 1) {
+      const isLastElement = i === n - 1;
+      result += text + (isLastElement ? '' : '\n');
+    }
+
+    return result;
+  }
 
   // bandomieji kintamieji
+  const str1 = 'labas';
+  const n1 = 3;
+  const str2 = 'ate';
+  const n2 = 2;
+  const str3 = 'gyvate';
+  const n3 = 7;
 
   // funkcijos iškvietimai ir saugojimai į rezultatus
+  const res1 = printStringNTimesInRow(str1, n1);
+  const res2 = printStringNTimesInRow(str2, n2);
+  const res3 = printStringNTimesInRow(str3, n3);
 
-  // rezultatų spausdinimas 
+  // rezultatų spausdinimas
+  console.log(res1);
+  console.log(res2);
+  console.log(res3);
 }
 console.groupEnd();
