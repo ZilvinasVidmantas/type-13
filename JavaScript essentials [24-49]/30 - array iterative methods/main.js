308ae65362882651a82b8fb6661ab62a59f678a3
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-22 10:27:13 +0200

    feat(30): tasks

---
+++ JavaScript essentials [24-49]/30 - array iterative methods/main.js
@@ -0,0 +1,80 @@
+const flats = [
+  {
+    address: 'Vilnakiemio g. 7',
+    rooms: [
+      { title: 'virtuvė', area: 12 },
+      { title: 'svetainė', area: 20 },
+      { title: 'tualetas', area: 8 },
+      { title: 'miegamasis', area: 16 },
+    ],
+    city: 'Vilnius',
+    price: 120000
+  },
+
+  {
+    address: 'Ruginių pr. 17',
+    rooms: [
+      { title: 'virtuvė', area: 18 },
+      { title: 'svetainė', area: 18 },
+      { title: 'tualetas', area: 8 },
+      { title: 'miegamasis', area: 12 },
+      { title: 'miegamasis2', area: 16 },
+    ],
+    city: 'Kaunas',
+    price: 120000
+  },
+
+  {
+    address: 'Saulės g. 7',
+    rooms: [
+      { title: 'virtuvė', area: 12 },
+      { title: 'svetainė', area: 20 },
+      { title: 'tualetas', area: 8 },
+    ],
+    city: 'Klaipėda',
+    price: 80000
+  },
+];
+
+// Kiekvienai užduočiai padeklaruokite funkciją, kurią panaudosite kviestami metodą Array.prototype.forEach
+
+console.group('1. Atspausdinkite kiekvieno buto adresą su miestu');
+{
+
+}
+console.groupEnd();
+
+console.group('2. Atspausdinkite kiekvieno buto kambarių skaičių');
+{
+
+}
+console.groupEnd();
+
+console.group('3. Atspausdinkite kiekvieno buto bendrą plotą');
+{
+
+}
+console.groupEnd();
+
+console.group('4. Atspausdinkite kiekvieno buto eilutes, kuriose kableliais atskirti kambarių pavadinimai');
+{
+
+}
+console.groupEnd();
+
+console.group('5. Atspausdinkite kiekvieno buto kvadrato kainą');
+{
+
+}
+console.groupEnd();
+
+console.group('6. Atspausdinkite kiekvieno buto kvadrato kainą ir adresą formatu "<address>, <city>: <kvadrato kaina>"');
+{
+
+}
+console.groupEnd();
+
+
+
+
+
