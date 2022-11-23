787aea5ac8ba92710a0e39a9cc3d433a4ac817e9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-23 08:03:24 +0200

    chore(31): minor changes

--- JavaScript essentials [24-49]/31 - array iterative methods 2/funtions-tasks-1-11.js
+++
@@ -1,9 +0,0 @@
-function round(number) {
-  const remainder = number % 1;
-  const whole = number - remainder;
-
-  if (remainder <= -0.5) return whole - 1;
-  else if (remainder >= 0.5) return whole + 1;
-
-  return whole;
-}
