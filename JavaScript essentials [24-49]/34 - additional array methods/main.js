6f24e6d1c7e67ba8b3c679e95d21a62769c9ec1a
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-28 09:35:22 +0200

    feat(34): indexOf and lastIndexOf tasks

--- JavaScript essentials [24-49]/34 - additional array methods/main.js
+++ JavaScript essentials [24-49]/34 - additional array methods/main.js
@@ -1,95 +1,14 @@
-const flats = [{
-  address: 'Vilnakiemio g. 7',
-  rooms: [
-    { title: 'virtuvė', area: 12 },
-    { title: 'svetainė', area: 20 },
-    { title: 'tualetas', area: 8 },
-    { title: 'miegamasis', area: 16 },
-  ],
-  city: 'Vilnius',
-  price: 120000
-}, {
-  address: 'Ruginių pr. 17',
-  rooms: [
-    { title: 'virtuvė', area: 18 },
-    { title: 'svetainė', area: 18 },
-    { title: 'tualetas', area: 8 },
-    { title: 'miegamasis', area: 12 },
-    { title: 'miegamasis2', area: 16 },
-  ],
-  city: 'Kaunas',
-  price: 120000
-}, {
-  address: 'Valatkų pr. 17',
-  rooms: [
-    { title: 'virtuvė', area: 18 },
-    { title: 'miegamasis', area: 12 },
-    { title: 'miegamasis2', area: 16 },
-    { title: 'miegamasis2', area: 19 },
-  ],
-  city: 'Kaunas',
-  price: 120000
-}, {
-  address: 'Vareikų pr. 17',
-  rooms: [
-    { title: 'virtuvė', area: 18 },
-    { title: 'svetainė', area: 18 },
-    { title: 'miegamasis2', area: 16 },
-  ],
-  city: 'Kaunas',
-  price: 90000
-}, {
-  address: 'Majonezo pr. 17',
-  rooms: [
-    { title: 'tualetas', area: 8 },
-    { title: 'miegamasis', area: 12 },
-    { title: 'miegamasis2', area: 16 },
-  ],
-  city: 'Kaunas',
-  price: 50000
-}, {
-  address: 'Saulės g. 7',
-  rooms: [
-    { title: 'virtuvė', area: 12 },
-    { title: 'svetainė', area: 20 },
-    { title: 'tualetas', area: 8 },
-  ],
-  city: 'Klaipėda',
-  price: 80000
-}];
+const numbers = [2, 4, -7, 2, 5, 3, 6, 1, 2, 8, 4, -9, 2, 3, 4, 5, 3, 1];
 
-console.group('1. Patikrinkite, ar yra butų, su daugiau nei 5 kambariais');
-{
-  // Array.prototype.some
-}
-console.groupEnd();
 
-console.group('2. Patikrinkite, ar visi butai brangesni už 50 000');
-{
-  // Array.prototype.every
-}
-console.groupEnd();
+const indexesOfNumber3 = [];
+let index = -1;
+do {
+  index = numbers.indexOf(3, index + 1);
+  if (index === -1) {
+    break;
+  }
+  indexesOfNumber3.push(index);
+} while (true);
 
-console.group('3. Suraskite pirmą butą, kuris pigesnis už 55 000 ir jį atspausdinkite');
-{
-  // Array.prototype.find
-}
-console.groupEnd();
-
-console.group('4. Suraskite pirmą butą, kuris pigesnis nei 90 000 ir po jo įterpkite naują butą <flatToAdd>');
-{
-  const flatToAdd = {
-    address: 'Naujakurių g. 7',
-    rooms: [
-      { title: 'virtuvė', area: 12 },
-      { title: 'svetainė', area: 20 },
-      { title: 'tualetas', area: 8 },
-    ],
-    city: 'Jurbarkas',
-    price: 35000
-  };
-
-  // Array.prototype.findIndex
-  // Array.prototype.splice(nuoKurioIndex, kiekIstrinti, ...iterpiamiElementai)
-}
-console.groupEnd();
+console.log(indexesOfNumber3);
