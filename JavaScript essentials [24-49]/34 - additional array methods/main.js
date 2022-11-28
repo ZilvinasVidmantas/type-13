37be1ae2017b56435317e502524d6531b4a62da9
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-11-28 10:13:04 +0200

    chore(34): moved tasks to task folder

--- JavaScript essentials [24-49]/34 - additional array methods/main.js
+++ JavaScript essentials [24-49]/34 - additional array methods/main.js
@@ -1,5 +1,5 @@
+// finding all element indexes;
 const numbers = [2, 4, -7, 2, 5, 3, 6, 1, 2, 8, 4, -9, 2, 3, 4, 5, 3, 1];
-
 
 const indexesOfNumber3 = [];
 let index = -1;
@@ -12,3 +12,9 @@ do {
 } while (true);
 
 console.log(indexesOfNumber3);
+
+// Splice insert example
+const foundElementIndex = 3;
+const arr = [1, 5, 3, 4, 8, 9];
+arr.splice(foundElementIndex + 1, 0, 777)
+console.log(arr);
