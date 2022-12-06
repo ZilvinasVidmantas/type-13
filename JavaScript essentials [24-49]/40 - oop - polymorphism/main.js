dad15a360644042546e3b5f1c1d9565666b6c446
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-06 11:10:04 +0200

    feat(40): not so easy polymorphism task

--- JavaScript essentials [24-49]/40 - oop - polymorphism/main.js
+++ JavaScript essentials [24-49]/40 - oop - polymorphism/main.js
@@ -1,38 +1,39 @@
-class Rectangle {
-  x;
-  y;
+/*
+Jūsų praktikos darbo esmė, sukurti metodą vienodu pavadinimu skirtingose klasėse. Kiekviena konkreti klasė (SelfEmployedPerson, WorkPerson ir BuisnessLicencePerson) turi savaip apskaičiuoti užmokestį:
 
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-class Circle {
-  r;
+Turime 3 darbuotojų tipus:
+  * self-employed-person - darbuotojas dirbantis pagal individualią veiklą, kuris gauna pinigus už išdirbtas valandas ir valandinį užmokestį;
+  * work-person - žmogus kuri gauna atlygį vieną kart per mėnesį už kiekvieną darbo dieną;
+  * business-license-person - žmogus kuris gauna atlygį už atliktus darbus;
 
-  constructor(r) {
-    this.r = r;
-  }
-}
+  KLASĖS:
+    * Person - bendros visų darbuotojų savybės
+      * name
+      * surname
 
-const shapes = [
-  new Rectangle(5, 10),
-  new Rectangle(7, 7),
-  new Circle(5),
-  new Circle(7),
-  new Circle(9),
-];
+    * WorkPerson - pagal darbo dienas mėnesyje
+      * hourPay
+      * fullTimeEquivalent
+      * calcPay()
 
-console.group('1. Sukurkite figūroms perimetro apskaičiavimo funkciją ir panaudokite ją polimorfiškai');
-{
+    * SelfEmployedPerson - pagal išdirbtas valandas
+      * hourPay
+      * hoursWorked
+      * calcPay()
 
-}
-console.groupEnd();
+    * BuisnessLicencePerson - pagal atliktus darbus (Job)
+      * jobs
+      * calcPay()
 
-console.group('2. Sukurkite figūroms ploto apskaičiavimo funkciją ir panaudokite ją polimorfiškai');
-{
+    * Job - klasė kurią naudos BuisnessLicencePerson
+      * id
+      * finished
+      * payed
 
-}
-console.groupEnd();
+  Darbui atlikti galite sukurti papildomas savo savybes ir metodus
 
+  Išsaugokite visus darbuotojus viename masyve ir atspausdinkite visų darbuotojų atlyginimus naudodami metodą calcPay()
+*/
 
+// const people = [...];
+// const pays = people.map(p => p.calcPay); // Array<number>;
