76db07581fd21997d26dfa898a0af6f39af44371
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-07 11:02:56 +0200

    feat(41): BuisnessLicencePerson.calcPay implemented

--- JavaScript essentials [24-49]/41 - oop - polymorphism 2/main.js
+++ JavaScript essentials [24-49]/41 - oop - polymorphism 2/main.js
@@ -111,6 +111,60 @@ class SelfEmployedPerson {
   }
 }
 
+class Job {
+  static count = 0;
+
+  id;
+  title;
+  amount;
+  finished;
+
+  constructor(title, amount) {
+    Job.count += 1;
+    this.id = `${Job.count}_${title.slice(0, 10)}`;
+    this.title = title;
+    this.amount = amount;
+    this.finished = false;
+  }
+}
+
+class BuisnessLicencePerson {
+  jobs;
+  payedJobs;
+
+  constructor(jobs) {
+    this.jobs = jobs ?? [];
+    this.payedJobs = [];
+  }
+
+  assignJob(job) {
+    this.jobs.push(job);
+  }
+
+  completeJob(jobId) {
+    const jobToComplete = this.jobs.find(job => job.id === jobId);
+    if (jobToComplete !== undefined) {
+      jobToComplete.finished = true;
+    }
+  }
+
+  calcPay() {
+    const finishedJobs = this.jobs.filter(job => job.finished);
+    if (finishedJobs.length > 0) {
+      this.payedJobs.splice(this.payedJobs.length, 0, ...finishedJobs);
+
+      const finishedJobsIds = finishedJobs.map(job => job.id);
+      this.jobs = this.jobs.filter(job => !finishedJobsIds.includes(job.id));
+
+      return finishedJobs.reduce((prevSum, job) => prevSum + job.amount, 0);
+    }
+
+    return 0;
+  }
+}
+
+
+
 // -------------------------------- Examples ---------------------------------
 
 const workers = [
@@ -121,10 +175,20 @@ const workers = [
 ];
 
 const selfEmployedPeople = [
-  new SelfEmployedPerson(30),
-  new SelfEmployedPerson(5),
-  new SelfEmployedPerson(12),
+  new SelfEmployedPerson(30), // 0
+  new SelfEmployedPerson(5),  // 1
+  new SelfEmployedPerson(12), // 2
 ];
+
+const buisnessLicencePeople = [
+  new BuisnessLicencePerson([
+    new Job('Sukurti mygtuką su 3 spalvų variacijomis', 60),
+    new Job('Sukurti šabloną su išskleidžiamu šoniniu meniu', 220),
+    new Job('Sukurti produktų parsiuntimo ir atvaizdavimo puslapį', 300),
+  ]),
+  new BuisnessLicencePerson(),
+  new BuisnessLicencePerson(),
+]
 
 console.group('WorkPerson atlyginimai');
 {
@@ -135,7 +199,6 @@ console.groupEnd();
 
 console.group('SelfEmployedPerson atlyginimai');
 {
-
   selfEmployedPeople[0].logHours(180);
   selfEmployedPeople[0].logHours(40);
 
@@ -146,6 +209,20 @@ console.group('SelfEmployedPerson atlyginimai');
   selfEmployedPeople[2].logHours(10);
   selfEmployedPeople[2].logHours(20);
 
-  selfEmployedPeople.forEach(w => console.log(w.calcPay()))
+  selfEmployedPeople.forEach(w => console.log(w.calcPay()));
+}
+console.groupEnd();
+
+console.group('BuisnessLicencePerson atlyginimai');
+{
+  buisnessLicencePeople[0].assignJob(new Job('Sukurti autentifikacijos formas', 400));
+  buisnessLicencePeople[0].completeJob('2_Sukurti ša');
+  buisnessLicencePeople[0].completeJob('3_Sukurti pr');
+
+  buisnessLicencePeople[1].assignJob(new Job('Sukurti banerių skečus', 100));
+  buisnessLicencePeople[1].assignJob(new Job('Retušuoti sliderio nuotraukas', 200));
+  buisnessLicencePeople[1].assignJob(new Job('Sukurti žaismingą spalvinė gama About puslapiui', 200));
+
+  buisnessLicencePeople.forEach(w => console.log(w.calcPay()));
 }
 console.groupEnd();
