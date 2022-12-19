4fe002055231ccab8b2a19393f28bb375dbe0095
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-19 12:00:57 +0200

    feat(49): homework

--- JavaScript DOM [49-57]/49 - DOM object tree intro/main.js
+++ JavaScript DOM [49-57]/49 - DOM object tree intro/main.js
@@ -1,19 +1,25 @@
 const resultContainer = document.querySelector('.js-result-container');
 const btnAdd = document.querySelector('.js-btn-add');
+const btnClear = document.querySelector('.js-btn-clear');
+const btnDeleteLast = document.querySelector('.js-btn-delete-last');
 
 const createNewDiv = () => {
   const newDiv = document.createElement('div');
   newDiv.innerHTML = 'Idetas tekstas';
   resultContainer.append(newDiv);
-}
+};
 
+const clearResultContainer = () => {
+  resultContainer.innerHTML = '';
+};
+// TODO: HTMLElement.children  vs HTMLElement.childNodes
+
+const deleteLast = () => {
+  if (resultContainer.childElementCount > 0) {
+    resultContainer.childNodes[resultContainer.childElementCount - 1].remove();
+  }
+};
 
 btnAdd.addEventListener('click', createNewDiv);
-
-/*
-  Suraskite ištrynimo mygtuką <document> kintamajame ir padarykite,
-  kas paspaudus jį, išsitrintų visas <resultContainer> turinys.
-
-  turinio ištrynimo komanda:
-  * resultContainer.innerHTML = ''
-*/
+btnClear.addEventListener('click', clearResultContainer);
+btnDeleteLast.addEventListener('click', deleteLast);
