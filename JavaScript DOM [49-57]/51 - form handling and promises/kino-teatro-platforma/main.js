c1041d7c2406b28697a9ec62be8dbf58be1dfced
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-21 10:15:01 +0200

    feat(51): instructions for form tasks

--- JavaScript DOM [49-57]/51 - form handling and promises/kino-teatro-platforma/main.js
+++ JavaScript DOM [49-57]/51 - form handling and promises/kino-teatro-platforma/main.js
@@ -1,3 +1,4 @@
+// 2.1
 const formPriceByAge = document.querySelector('.js-form-price-by-age');
 const inputAge = formPriceByAge.querySelector('.js-input-age');
 const invalidFeedbackInputAge = formPriceByAge.querySelector('.js-invalid-feedback-input-age');
@@ -26,13 +27,9 @@ const hideAgeError = () => {
 };
 
 // Kviečiama sėkmės atveju
-const setPriceElementTextContent = (text) => {
-  priceElement.textContent = text;
-};
-
-// Kviečiama sėkmės atveju
-const displayPriceContainer = () => {
+const displayPrice = (text) => {
   priceContainer.classList.remove('d-none');
+  priceElement.textContent = text;
 };
 
 // Kviečiama NE sėkmės atveju
@@ -41,30 +38,43 @@ const hidePriceContainer = () => {
 };
 
 const handleFormPriceByAgeSubmitSuccess = async (validatedAge) => {
+  // 2.3.5.1.1
   const price = await API.getPriceByAge(validatedAge);
+  // 2.3.5.1.2
   hideAgeError();
-  setPriceElementTextContent(price);
-  displayPriceContainer();
+  // 2.3.5.1.3
+  displayPrice(price);
 };
 
 const handleFormPriceByAgeSubmitFailure = (errorMsg) => {
-  hidePriceContainer();
+  // 2.3.5.2.1
   displayAgeError(errorMsg);
+  // 2.3.5.2.2
+  hidePriceContainer();
 };
 
-// Controller
+// 2.3 - Controller
 const handleFormPriceByAgeSubmit = (event) => {
+  // 2.3.1
   event.preventDefault();
+  // 2.3.2
   const formData = new FormData(event.target);
 
+  // 2.3.3
   const age = formData.get('age');
+
+  // 2.3.4
   const validatedAgeOrErrorMsg = getValidatedAge(age);
 
+  // 2.3.5
   if (typeof validatedAgeOrErrorMsg !== 'string') {
+    // 2.3.5.1
     handleFormPriceByAgeSubmitSuccess(validatedAgeOrErrorMsg);
   } else {
+    // 2.3.5.2
     handleFormPriceByAgeSubmitFailure(validatedAgeOrErrorMsg);
   }
 };
 
+// 2.2
 formPriceByAge.addEventListener('submit', handleFormPriceByAgeSubmit);
