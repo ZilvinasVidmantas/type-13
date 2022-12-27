b4c6231e6f852b5b1d3a16739bfef174e0558705
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-27 10:10:27 +0200

    feat(54): add submit event listener for TodoFormComponent

--- JavaScript DOM [49-57]/54 - FS CRUD app - create/client/components/concrete/todo-form-component.js
+++ JavaScript DOM [49-57]/54 - FS CRUD app - create/client/components/concrete/todo-form-component.js
@@ -8,13 +8,36 @@ class TodoFormComponent {
     <h2 class="h2 text-center mb-4 fw-normal">Create Todo</h2>
     <div class="mb-3">
       <label for="todo-title" class="form-label">Title</label>
-      <input type="email" class="form-control" id="todo-title">
+      <input type="text" class="form-control" id="todo-title" name="title">
     </div>
     <div class="mb-3 form-check">
-      <input type="checkbox" class="form-check-input" id="todo-done">
+      <input type="checkbox" class="form-check-input" id="todo-done" name="done">
       <label class="form-check-label" for="todo-done">Job is done</label>
     </div>
     <button type="submit" class="btn btn-primary w-100">Create</button>`;
+
+    this.htmlElement.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      // Forma privalo turėti [name] atributus
+      const formData = new FormData(event.target);
+      const title = formData.get('title');
+      const done = formData.get('done') === 'on';
+
+      // Validacija
+
+
+      // Validacija sekminga
+
+
+      // validacija nesekminga
+
+      const dataForTodoCreation = { title, done };
+
+      console.log(dataForTodoCreation)
+
+
+    });
   }
 }
 
