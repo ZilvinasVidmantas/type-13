3a957416d9019bc10b59983464314a11efffbab5
Zilvinas Vidmantas <zilvinas.vidmantas.93@gmail.com>
2022-12-28 10:25:19 +0200

    feat(55): row data edit UI/UX implemented

--- JavaScript DOM [49-57]/55 - FS CRUD app - update/client/components/concrete/todo-table-component.js
+++ JavaScript DOM [49-57]/55 - FS CRUD app - update/client/components/concrete/todo-table-component.js
@@ -22,24 +22,83 @@ class TodoTableComponent {
     this.renderTodos(todos);
   }
 
+  enableRowEditAction = ({ tr, initialState }) => {
+    let isBeingEdited = false;
+    const titleColumn = tr.querySelector('.js-title-col');
+    const doneCheckbox = tr.querySelector('.js-done-col input[type=checkbox]');
+    const doneLabel = tr.querySelector('.js-done-col label');
+
+    const cancelEditing = () => {
+      tr.classList.remove('todo-table__row--editable');
+      updateButton.innerHTML = '⟳';
+      updateButton.classList.replace('btn-info', 'btn-warning');
+      titleColumn.setAttribute('contenteditable', 'false');
+      titleColumn.textContent = initialState.title;
+      doneCheckbox.checked = initialState.done;
+      doneLabel.textContent = initialState.done ? 'Yes' : 'No';
+      isBeingEdited = false;
+    }
+
+    const enableEditing = () => {
+      tr.classList.add('todo-table__row--editable');
+      updateButton.innerHTML = '⦰';
+      updateButton.classList.replace('btn-warning', 'btn-info');
+      titleColumn.setAttribute('contenteditable', 'true');
+      doneCheckbox.checked = initialState.done;
+      isBeingEdited = true;
+    }
+
+    document.addEventListener('click', (e) => {
+      e.stopPropagation();
+      // Jeigu paspaustas elementas NĖRA eilutės vaikas -> atšaukti redagavimą
+      if (!tr.contains(e.target)) cancelEditing();
+    });
+
+    doneCheckbox.addEventListener('change', () => {
+      doneLabel.textContent = doneCheckbox.checked ? 'Yes' : 'No';
+    })
+
+    const updateButton = tr.querySelector('.btn-warning');
+    updateButton.addEventListener('click', () => {
+      if (isBeingEdited) cancelEditing()
+      else enableEditing();
+    });
+
+  }
+
+  enableRowDeleteAction = ({ id, initialState, tr }) => {
+    const delButton = tr.querySelector('.btn-danger');
+    delButton.addEventListener('click', () => this.onDeleteTodo({ id, title: initialState.title }));
+  }
+
+  enableRowUpdateAction = ({ id, tr }) => {
+    const updateButton = tr.querySelector('.btn-success');
+    updateButton.addEventListener('click', () => {
+      console.log('Duomemys siunčiami atnaujinimui', id);
+    });
+  }
+
   createRowHtmlElement = ({ id, title, done }) => {
     const tr = document.createElement('tr');
+    tr.className = 'todo-table__row';
     tr.innerHTML = `
       <td>${id}</td>
-      <td>${title}</td>
-      <td>${done ? 'Yes' : 'No'}</td>
+      <td class="js-title-col">${title}</td>
+      <td class="js-done-col">
+        <input type="checkbox" id="${id}"/>
+        <label for="${id}">${done ? 'Yes' : 'No'}</label>
+      </td>
       <td class="d-flex justify-content-end gap-2 todo-table__last-cell">
+        <button class="btn btn-success btn-sm">✓</button>
         <button class="btn btn-warning btn-sm">⟳</button>
         <button class="btn btn-danger btn-sm">✕</button>
       </td>`;
 
-    const delButton = tr.querySelector('.btn-danger');
-    delButton.addEventListener('click', () => this.onDeleteTodo({ id, title }));
+    const actionProps = { id, tr, initialState: { title, done } };
 
-    const updateButton = tr.querySelector('.btn-warning');
-    updateButton.addEventListener('click', () => {
-      console.log(id);
-    });
+    this.enableRowDeleteAction(actionProps);
+    this.enableRowEditAction(actionProps);
+    this.enableRowUpdateAction(actionProps);
 
     return tr;
   }
